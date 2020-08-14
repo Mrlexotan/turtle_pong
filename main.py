00a0d47d8e3b5f363c2d8cc4e009b282139ac618
Mr.Lexotan <leite.tavares.joao@gmail.com>
2020-08-14 05:23:26 -0300

    Adiciona os controles "w" para subir "s" para descer e, define o movimento de subida e descida

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import turtle
 
-#Janela do jogo
+# Janela do jogo
 window = turtle.Screen()
 window.setup(1200, 800)
 window.title("Turtle Pong")
 window.bgcolor("black")
 
-#player1
+# player1
 player1 = turtle.Turtle()
 player1.penup()
 player1.speed(0)
@@ -16,7 +16,7 @@ player1.color("white")
 player1.goto(-500, 0)
 player1.shapesize(8, 1)
 
-#player2
+# player2
 player2 = turtle.Turtle()
 player2.penup()
 player2.speed(0)
@@ -25,7 +25,7 @@ player2.color("white")
 player2.goto(500, 0)
 player2.shapesize(8, 1)
 
-#ball
+# ball
 ball = turtle.Turtle()
 ball.penup()
 ball.speed(0)
@@ -34,7 +34,32 @@ ball.color("white")
 ball.goto(0, 0)
 ball.shapesize(1, 1)
 
-#Mantém a janela aberta
+
+# define o movimento de subida
+def up():
+    y = player1.ycor()
+    y += 10
+    player1.sety(y)
+
+    if player1.ycor() >= 320:
+        player1.sety(320)
+
+# define o movimento de descida
+def down():
+    y = player1.ycor()
+    y -= 10
+    player1.sety(y)
+
+    if player1.ycor() <= -320:
+        player1.sety(-320)
+
+
+# adicionando os controles
+window.listen()
+window.onkeypress(up, "w")
+window.onkeypress(down, "s")
+
+# Mantém a janela aberta
 loop = True
 while loop:
     window.update()
